724218faa9c776bb441ad020abed9f327451b84c
жора <misha.fokin.04@mail.ru>
2024-09-05 08:59:55 +0300

    chore: updated version

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: '/chat',
   build: {
-    assetsDir: 'assets_0.0.05',
+    assetsDir: 'assets_0.0.06',
   },
   plugins: [svgr(), react(), tsconfigPaths()],
 });

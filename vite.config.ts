29cde0b04bfe75a53c13da5f6bee7b7b68447da1
godtyze <64466434+godtyze@users.noreply.github.com>
2024-09-23 16:56:24 +0600

    chore: update assets version

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: '/chat',
   build: {
-    assetsDir: 'assets_0.0.12',
+    assetsDir: 'assets_0.0.13',
   },
   plugins: [svgr(), react(), tsconfigPaths()],
 });

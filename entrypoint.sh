ac7d87d6917b02b2854459c19d2bb4aafa89f5d5
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-07-10 09:12:32 +0500

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-BUILD_DIR="/usr/share/nginx/html/assets_0.0.59"
+
+# Находим директорию с префиксом "assets_" в /usr/share/nginx/html/
+ASSETS_DIR=$(find /usr/share/nginx/html -maxdepth 1 -type d -name 'assets_*' | head -n 1)
+
+if [ -z "$ASSETS_DIR" ]; then
+  echo "Не найдена директория assets_* в /usr/share/nginx/html/"
+  exit 1
+fi
+
+echo "Найдена директория: $ASSETS_DIR"
+BUILD_DIR="$ASSETS_DIR"
 
 # Перечисляем переменные, которые хотим подставить в JS-файлы
 VARIABLES="VITE_APP_API_URL"

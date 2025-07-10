9aaa6aba9de8ef2e590c2015c0df2ebb39e2847f
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-07-10 14:05:29 +0500

    feat: CSL-553

--- src/components/full-chat/content-side/contact-chat/contact-chat-header.component.tsx
+++ src/components/full-chat/content-side/contact-chat/contact-chat-header.component.tsx
@@ -4,25 +4,28 @@ import { CloseOutlined } from '@ant-design/icons';
 import { Space } from 'antd';
 import { Header } from 'antd/es/layout/layout';
 
+import waChatIcon from 'assets/wa-chat.svg';
 import AvatarImage from 'components/UI/avatar-image.component';
 import { useActions, useAppSelector } from 'hooks';
 import { selectActiveChat } from 'store/slices/chat.slice';
 import { ActiveChat } from 'types';
+import { isWhatsAppOfficialChat } from 'utils';
 
 const ContactChatHeader: FC = () => {
   const activeChat = useAppSelector(selectActiveChat) as ActiveChat;
-
   const { setActiveChat, setContactInfoOpen } = useActions();
+
+  const isOfficial = isWhatsAppOfficialChat(activeChat.chatId);
 
   return (
     <Header className="contact-chat-header">
       <Space className="chatHeader-space" onClick={() => setContactInfoOpen(true)}>
-        <AvatarImage src={activeChat.avatar} size="large" />
-        <h3 className="text-overflow">{activeChat.senderName}</h3>
+        <AvatarImage src={isOfficial ? waChatIcon : activeChat.avatar} size="large" />
+        <h3 className="text-overflow">{isOfficial ? 'WhatsApp' : activeChat.senderName}</h3>
       </Space>
 
       <Space>
-        {activeChat.chatId?.includes('@c') && (
+        {!isOfficial && activeChat.chatId?.includes('@c') && (
           <span>{activeChat.chatId?.replace(/\@.*$/, '')}</span>
         )}
         <a>

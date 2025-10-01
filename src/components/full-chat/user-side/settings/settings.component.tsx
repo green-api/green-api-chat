efc27212cf505336bc8e095d8c7525bb4ad565a9
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-10-01 11:02:56 +0500

    feat: CSL-614

--- src/components/full-chat/user-side/settings/settings.component.tsx
+++ src/components/full-chat/user-side/settings/settings.component.tsx
@@ -1,45 +1,16 @@
-import { Avatar, Flex } from 'antd';
+import { Flex } from 'antd';
 import { useTranslation } from 'react-i18next';
 
-import InstanceDangerZone from 'components/instance-danger-zone.component';
+import { Logout } from './logout.component';
+// import { Profile } from './profile.component';
 import SelectInstance from 'components/UI/select/select-instance.component';
-import { useActions, useAppSelector } from 'hooks';
-import { useIsMaxInstance } from 'hooks/use-is-max-instance';
-import {
-  useGetAccountSettingsQuery,
-  useGetWaSettingsQuery,
-  useLogoutMutation,
-} from 'services/green-api/endpoints';
+import { useAppSelector } from 'hooks';
 import { selectUserSideActiveMode } from 'store/slices/chat.slice';
-import { selectInstance } from 'store/slices/instances.slice';
-import { Profile } from './profile.component';
 
 const Settings = () => {
   const { t } = useTranslation();
 
-  const isMax = useIsMaxInstance();
-
   const active = useAppSelector(selectUserSideActiveMode);
-  const selectedInstance = useAppSelector(selectInstance);
-  const { setUserSideActiveMode } = useActions();
-
-  const [logoutInstance, { isLoading: isLogouting }] = useLogoutMutation();
-
-  const { data: waSettings } = useGetWaSettingsQuery(
-    {
-      ...selectedInstance,
-    },
-    { skip: !selectedInstance?.idInstance || !selectedInstance?.apiTokenInstance || isMax }
-  );
-
-  const { data: accountSettings } = useGetAccountSettingsQuery(
-    {
-      ...selectedInstance,
-    },
-    { skip: !selectedInstance?.idInstance || !selectedInstance?.apiTokenInstance || !isMax }
-  );
-
-  const settings = isMax ? accountSettings : waSettings;
 
   if (active === 'instance') {
     return (
@@ -50,30 +21,12 @@ const Settings = () => {
     );
   }
 
-  if (active === 'profile') {
-    return <Profile />;
-  }
+  // if (active === 'profile') {
+  //   return <Profile />;
+  // }
 
   if (active === 'logout') {
-    return (
-      <Flex className="settings" vertical gap={10}>
-        <p style={{ fontSize: '1.5rem' }}>{t('LOGOUT')}</p>
-        <InstanceDangerZone
-          onLogout={() => {
-            logoutInstance({
-              idInstance: selectedInstance.idInstance,
-              apiTokenInstance: selectedInstance.apiTokenInstance,
-              apiUrl: selectedInstance?.apiUrl,
-              mediaUrl: selectedInstance?.mediaUrl,
-            });
-            setUserSideActiveMode('chats');
-          }}
-          isLogoutDisabled={isLogouting}
-          isLogouting={isLogouting}
-          instanceStatus={settings?.stateInstance}
-        />
-      </Flex>
-    );
+    return <Logout />;
   }
 
   return null;

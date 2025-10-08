c81b332ae7bd545667adf166e24dc186e4b47644
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-10-08 16:08:58 +0500

    rm console logs

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -2,15 +2,14 @@ import { FC } from 'react';
 
 import ContactChat from 'components/full-chat/content-side/contact-chat/contact-chat.component';
 import HomeView from 'components/full-chat/content-side/home-view.component';
+import { AuthInstance } from 'components/instance-auth/instance-auth.component';
 import { useAppSelector } from 'hooks';
 import { selectActiveChat } from 'store/slices/chat.slice';
 import { selectIsAuthorizingInstance } from 'store/slices/instances.slice';
-import { AuthInstance } from 'components/instance-auth/instance-auth.component';
 
 const Main: FC = () => {
   const activeChat = useAppSelector(selectActiveChat);
   const isAurhorizingInstance = useAppSelector(selectIsAuthorizingInstance);
-  console.log(isAurhorizingInstance);
 
   if (isAurhorizingInstance) return <AuthInstance />;
 

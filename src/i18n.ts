73a7f88f1dfd6b166cdc196796f880a58bcbea3c
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-11-17 15:47:03 +0500

    fix: upgrade locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,7 +10,7 @@ i18n
   .init({
     fallbackLng: 'en',
     backend: {
-      loadPath: '/locales_0.0.67/{{lng}}/translation.json',
+      loadPath: '/locales_0.0.68/{{lng}}/translation.json',
       expirationTime: 24 * 60 * 60 * 1000,
     },
     detection: {

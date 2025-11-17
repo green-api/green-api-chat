9f9225985f01830c51f593324bb0b805a684f577
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2025-11-17 17:38:57 +0500

    fix: styles

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,18 +1,38 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 import ContactChat from 'components/full-chat/content-side/contact-chat/contact-chat.component';
 import HomeView from 'components/full-chat/content-side/home-view.component';
 import { AuthInstance } from 'components/instance-auth/instance-auth.component';
 import { MaxAuth } from 'components/instance-auth/max-auth.component';
 import { useAppSelector } from 'hooks';
-import { isMaxInstance, useIsMaxInstance } from 'hooks/use-is-max-instance';
+import { useIsMaxInstance } from 'hooks/use-is-max-instance';
 import { selectActiveChat } from 'store/slices/chat.slice';
 import { selectIsAuthorizingInstance } from 'store/slices/instances.slice';
+import { MessageEventTypeEnum } from 'types';
 
 const Main: FC = () => {
   const activeChat = useAppSelector(selectActiveChat);
   const isAuthorizingInstance = useAppSelector(selectIsAuthorizingInstance);
   const isMax = useIsMaxInstance();
+  const {
+    i18n: { resolvedLanguage },
+  } = useTranslation();
+
+  useEffect(() => {
+    if (resolvedLanguage) {
+      window.parent.postMessage(
+        {
+          type: MessageEventTypeEnum.LOCALE_CHANGE,
+          payload: {
+            locale: resolvedLanguage,
+          },
+        },
+        '*'
+      );
+    }
+  }, [resolvedLanguage]);
 
   if (isAuthorizingInstance) return isMax ? <MaxAuth /> : <AuthInstance />;
 

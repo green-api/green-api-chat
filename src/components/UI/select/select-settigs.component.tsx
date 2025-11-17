0be8fbc2326e66970476688b23816cb3f89f9963
Sergei Artemenko <130140499+artemenkoS@users.noreply.github.com>
2025-11-17 15:21:28 +0500

    feat: CSL-712 (#22)
    * feat: CSL-712
    
    * feat: CSL-712

--- src/components/UI/select/select-settigs.component.tsx
+++ src/components/UI/select/select-settigs.component.tsx
@@ -1,4 +1,4 @@
-import { SettingOutlined } from '@ant-design/icons';
+import { GlobalOutlined, SettingOutlined } from '@ant-design/icons';
 import { Dropdown, Flex } from 'antd';
 import { useTranslation } from 'react-i18next';
 
@@ -30,6 +30,15 @@ export const SettingsSelect = () => {
         <Flex gap={10} align="center">
           <ProfileIcon width={24} style={{ padding: '2px 0' }} />
           {t('PROFILE')}
+        </Flex>
+      ),
+    },
+    {
+      key: 'language',
+      label: (
+        <Flex gap={10} align="center">
+          <GlobalOutlined width={24} style={{ padding: '2px 4px', fontSize: 16 }} />
+          {t('LANGUAGE')}
         </Flex>
       ),
     },
--- src/configs/aside.config.tsx
+++ src/configs/aside.config.tsx
@@ -3,6 +3,7 @@ import { SettingOutlined } from '@ant-design/icons';
 import ChatIcon from 'assets/chat.svg?react';
 import Chats from 'components/full-chat/user-side/chats/chats.component';
 import { InstanceSettings } from 'components/full-chat/user-side/settings/instance.component';
+import { Language } from 'components/full-chat/user-side/settings/language.componet';
 import { Logout } from 'components/full-chat/user-side/settings/logout.component';
 import { Profile } from 'components/full-chat/user-side/settings/profile.component';
 import { SettingsSelect } from 'components/UI/select/select-settigs.component';
@@ -28,5 +29,6 @@ export const USER_SIDE_ITEMS: UserSideItem[] = [
   { item: 'chats', element: <Chats /> },
   { item: 'instance', element: <InstanceSettings /> },
   { item: 'profile', element: <Profile /> },
+  { item: 'language', element: <Language /> },
   { item: 'logout', element: <Logout /> },
 ];

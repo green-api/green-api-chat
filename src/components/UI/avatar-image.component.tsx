92379e535ef27ce8f391d68ab80b110cae9b4400
artemenkoS <130140499+artemenkoS@users.noreply.github.com>
2026-04-03 11:58:18 +0500

    upgrade locales

--- src/components/UI/avatar-image.component.tsx
+++ src/components/UI/avatar-image.component.tsx
@@ -1,16 +1,46 @@
-import { FC } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 
 import { Avatar } from 'antd';
 import { AvatarProps } from 'antd/es/avatar/avatar';
+
+import emptyAvatarButAvailable from 'assets/emptyAvatarButAvailable.svg';
 import { normalizeAvatarSrc } from 'utils';
 
 interface AvatarImageProps {
   src: string;
   size?: AvatarProps['size'];
+  fallbackSrc?: string;
 }
 
-const AvatarImage: FC<AvatarImageProps> = ({ src, size }) => {
-  return <Avatar src={normalizeAvatarSrc(src)} size={size} alt="avatar" />;
+const AvatarImage: FC<AvatarImageProps> = ({
+  src,
+  size,
+  fallbackSrc = emptyAvatarButAvailable,
+}) => {
+  const normalizedSrc = useMemo(() => normalizeAvatarSrc(src), [src]);
+  const normalizedFallbackSrc = useMemo(() => normalizeAvatarSrc(fallbackSrc), [fallbackSrc]);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    setHasError(false);
+  }, [normalizedSrc]);
+
+  const resolvedSrc = hasError ? normalizedFallbackSrc : normalizedSrc;
+
+  return (
+    <Avatar
+      src={resolvedSrc}
+      size={size}
+      alt="avatar"
+      onError={() => {
+        if (!hasError && normalizedFallbackSrc) {
+          setHasError(true);
+        }
+
+        return false;
+      }}
+    />
+  );
 };
 
 export default AvatarImage;

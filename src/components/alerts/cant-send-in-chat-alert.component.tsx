8295fe8a8bfb7aedb97d80a3720fd6cca0de8dd0
godtyze <omgfail301@gmail.com>
2026-09-25 01:00:51 +0600

    feat: add button "Go to new chat"

--- src/components/alerts/cant-send-in-chat-alert.component.tsx
+++ src/components/alerts/cant-send-in-chat-alert.component.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
-import { Flex, Typography } from 'antd';
+import { Button, Flex, Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
+
+import { useActions, useAppSelector } from 'hooks';
+import { selectActiveChat } from 'store/slices/chat.slice';
 
 export type CantSendInChatReason = 'group' | 'inactive-account';
 
@@ -16,12 +19,35 @@ interface CantSendInChatAlertProps {
 
 const CantSendInChatAlert: FC<CantSendInChatAlertProps> = ({ reason = 'group' }) => {
   const { t } = useTranslation();
+  const activeChat = useAppSelector(selectActiveChat);
+  const { setActiveChat } = useActions();
+
+  const handleGoToNewChat = () => {
+    if (!activeChat?.newChatId) return;
+
+    setActiveChat({
+      ...activeChat,
+      chatId: activeChat.newChatId,
+      newChatId: undefined,
+    });
+  };
 
   return (
-    <Flex align="center" justify="center" className="chat-form-container text-center p-10">
+    <Flex
+      vertical
+      align="center"
+      justify="center"
+      gap={12}
+      className="chat-form-container text-center p-10"
+    >
       <Typography.Paragraph style={{ margin: 'initial' }}>
         {t(REASON_TRANSLATION_KEYS[reason])}
       </Typography.Paragraph>
+      {reason === 'inactive-account' && activeChat?.newChatId && (
+        <Button type="primary" onClick={handleGoToNewChat}>
+          {t('GO_TO_NEW_CHAT')}
+        </Button>
+      )}
     </Flex>
   );
 };
